b932d2e10b0d4257bf1daf1cb92d564388edea9d
zhutian.chen <zhutian.chen.code@outlook.com>
2020-12-23 18:36:09 -0500

    style: format the code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,71 @@
-export { behavior as default } from './behavior'
+import { behavior } from "./behavior";
+
+export { behavior as default } from "./behavior";
+
+type ComputedInstance<
+  D extends WechatMiniprogram.Component.DataOption,
+  P extends WechatMiniprogram.Component.PropertyOption,
+  M extends WechatMiniprogram.Component.MethodOption,
+  C extends Record<string, (data: D & { [K in keyof P]: any }) => any>,
+  TCustomProperty extends WechatMiniprogram.IAnyObject = Record<string, never>
+> = WechatMiniprogram.Component.Instance<D, P, M, TCustomProperty> & {
+  data: { [K in keyof C]: ReturnType<C[K]> } & { [K in keyof P]: any };
+};
+
+type ComputedOptions<
+  TData extends WechatMiniprogram.Component.DataOption,
+  TProperty extends WechatMiniprogram.Component.PropertyOption,
+  TMethod extends WechatMiniprogram.Component.MethodOption,
+  TWatch extends Record<string, (...args: any[]) => void>,
+  TComputed extends Record<
+    string,
+    (data: TData & { [K in keyof TProperty]: any }) => any
+  >,
+  TCustomInstanceProperty extends WechatMiniprogram.IAnyObject = {}
+> = (Partial<WechatMiniprogram.Component.Data<TData>> &
+  Partial<WechatMiniprogram.Component.Property<TProperty>> &
+  Partial<WechatMiniprogram.Component.Method<TMethod>> &
+  Partial<WechatMiniprogram.Component.OtherOption> &
+  Partial<WechatMiniprogram.Component.Lifetimes> & {
+    watch?: TWatch;
+    computed?: TComputed;
+    template?: string;
+  }) &
+  ThisType<
+    ComputedInstance<
+      TData,
+      TProperty,
+      TMethod,
+      TComputed,
+      TCustomInstanceProperty
+    >
+  >;
+
+export function ComputedComponent<
+  TData extends WechatMiniprogram.Component.DataOption,
+  TProperty extends WechatMiniprogram.Component.PropertyOption,
+  TMethod extends WechatMiniprogram.Component.MethodOption,
+  TWatch extends Record<string, (...args: any[]) => void>,
+  TComputed extends Record<
+    string,
+    (data: TData & { [K in keyof TProperty]: any }) => any
+  >,
+  TCustomInstanceProperty extends WechatMiniprogram.IAnyObject = {}
+>(
+  options: ComputedOptions<
+    TData,
+    TProperty,
+    TMethod,
+    TWatch,
+    TComputed,
+    TCustomInstanceProperty
+  >
+): string {
+  if (!Array.isArray(options)) {
+    // assume that no other behaviors
+    options.behaviors = [];
+  }
+
+  options.behaviors.push(behavior);
+  return Component(options);
+}
