0daf5afb7228ac71e851e3da874f6765fd915245
zhutian.chen <zhutian.chen.code@outlook.com>
2020-12-23 18:36:15 -0500

    chore: update tool chain

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    preset: 'ts-jest',
+    bail: 1,
+    verbose: true,
+    testEnvironment: 'jsdom',
+    testURL: 'https://jest.test',
+    moduleFileExtensions: ['js', 'ts'],
+    testMatch: ['<rootDir>/test/**/*.spec.ts'],
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.ts',
+        '!**/__test__/**'
+    ],
+    snapshotSerializers: ['miniprogram-simulate/jest-snapshot-plugin']
+}
