fa9b9db16f8d4d2fbe75ec21207f87b4eb13e23c
RomChung <thelns@vip.qq.com>
2021-09-02 14:47:44 +0800

    fix: types deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,38 +2,38 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-      ecmaVersion: 6,
-      sourceType: 'module'
+    ecmaVersion: 6,
+    sourceType: 'module'
   },
   env: {
-      es6: true,
-      browser: true,
-      jest: true,
-      commonjs: true,
-      node: true
+    es6: true,
+    browser: true,
+    jest: true,
+    commonjs: true,
+    node: true
   },
   plugins: [
     '@typescript-eslint',
     'prettier'
   ],
   extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/eslint-recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier/@typescript-eslint'
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint'
   ],
   globals: {
-      wx: true,
-      App: true,
-      Page: true,
-      Component: true
+    wx: true,
+    App: true,
+    Page: true,
+    Component: true
   },
   rules: {
-      'prettier/prettier': 'error',
-      '@typescript-eslint/ban-ts-ignore': 'off',
-      '@typescript-eslint/no-empty-function': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-non-null-assertion':'off'
+    'prettier/prettier': 'error',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off'
   },
 }
