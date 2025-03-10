61174245ba4ecdfa29f802d9b5905ca15c7aa04a
boquanfu <boquanfu@tencent.com>
2025-03-10 20:48:00 +0800

    chore: build

--- types/behavior.d.ts
+++ types/behavior.d.ts
@@ -1,5 +1,32 @@
+import * as dataTracer from './data-tracer';
 import type * as adapter from 'glass-easel-miniprogram-adapter';
-export declare const behavior: WechatMiniprogram.Behavior.BehaviorIdentifier<WechatMiniprogram.Component.DataOption, WechatMiniprogram.Component.PropertyOption, WechatMiniprogram.Component.MethodOption, WechatMiniprogram.Component.BehaviorOption>;
+interface BehaviorData {
+    _computedWatchInit: ComputedWatchInitStatus;
+    [k: string]: any;
+}
+interface BehaviorExtend {
+    data: BehaviorData;
+    setData(d: Record<string, any>): void;
+    _computedWatchInfo: Record<string, ComputedWatchInfo>;
+}
+interface ComputedWatchInfo {
+    computedUpdaters: Array<(...args: unknown[]) => boolean>;
+    computedRelatedPathValues: Array<Array<dataTracer.RelatedPathValue>>;
+    watchCurVal: Array<unknown>;
+    watchDisabled: boolean;
+}
+declare enum ComputedWatchInitStatus {
+    CREATED = 0,
+    ATTACHED = 1,
+    DISABLE_WATCHES = 2,
+    ENABLE_WATCHES = 3,
+    CALL_WATCHES = 4
+}
+export declare const behavior: WechatMiniprogram.Behavior.BehaviorIdentifier<WechatMiniprogram.Component.DataOption, WechatMiniprogram.Component.PropertyOption, {
+    disableWatches(): void;
+    enableWatches(callWatchesImmediately: boolean): void;
+    triggerAllWatches(): void;
+}, WechatMiniprogram.Component.BehaviorOption>;
 export declare function computed<TComputedDefinition1 extends {
     [k: string]: (data: adapter.glassEasel.typeUtils.DataWithPropertyValues<TPrevData, TProperty>) => any;
 }, TPrevData extends adapter.glassEasel.typeUtils.DataList, TProperty extends adapter.glassEasel.typeUtils.PropertyList>(ctx: adapter.builder.BuilderContext<TPrevData, TProperty, any>, computedDefinition1: TComputedDefinition1): adapter.glassEasel.typeUtils.DataWithPropertyValues<TPrevData & {
@@ -64,4 +91,14 @@ export declare function computed<TComputedDefinition1 extends {
 } & {
     [k in keyof TComputedDefinition4]: ReturnType<TComputedDefinition4[k]>;
 }, TProperty>;
-export declare const watch: (ctx: adapter.builder.BuilderContext<any, any, any>, watchPath: string, listener: (...args: any[]) => void) => void;
+export declare const generateWatches: (self: BehaviorExtend) => {
+    disableWatches(): void;
+    enableWatches(triggerWatchesImmediately: boolean): void;
+    triggerAllWatches(): void;
+};
+export declare const watch: (ctx: adapter.builder.BuilderContext<any, any, any>, watchPath: string, listener: (...args: any[]) => void) => {
+    disableWatches(): void;
+    enableWatches(triggerWatchesImmediately: boolean): void;
+    triggerAllWatches(): void;
+};
+export {};
